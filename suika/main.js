a7c2f4736e1f9e7a44e4dc786803f131df9df57a
pjs060708 <pjs060708@gmail.com>
2024-06-20 14:00:29 +0900

    background

---
+++ suika/main.js
@@ -0,0 +1,46 @@
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    World = Matter.World;
+
+const engine = Engine.create();
+
+const render = Render.create({
+    engine,
+    element:document.body,
+    options:{
+        wireframes: false,
+        background: '#F7F4CB',
+        width: 620,
+        height: 850,
+    }
+});
+
+const world = engine.world;
+
+const rightWall = Bodies.rectangle(605, 395, 30, 790, { 
+    isStatic: true, 
+    render: { 
+        fillStyle: '#6B143' 
+    }
+});
+
+const ground = Bodies.rectangle(310, 820, 620, 60, {
+     isStatic: true, 
+     render: { 
+        fillStyle: '#6B143' 
+    }
+});
+
+const topLine = Bodies.rectangle(310, 150, 620, 2, { 
+    isStatic: true, 
+    render: { 
+        fillStyle: '#6B143' 
+    }
+});
+
+World.add(world, [leftWall, rightWall, ground, topLine]);
+
+Render.run(render);
+Render.run(engine);
