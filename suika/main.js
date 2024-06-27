d0c4beecbbb801c48517a0c2bf4fd62def7dffe7
pjs060708 <pjs060708@gmail.com>
2024-06-27 13:44:57 +0900

    test

--- suika/main.js
+++ suika/main.js
@@ -1,11 +1,12 @@
-import { FRUITS } from "/suika/fruits.js";
+import {FRUITS} from "/suika/fruits.js";
 
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
     World = Matter.World,
-    Body = Matter.Body;
+    Body = Matter.Body,
+    Events = Matter.Events;
 
 // 엔진 선언
 const engine = Engine.create();
@@ -15,8 +16,8 @@ const render = Render.create({
     engine,
     element : document.body,
     options : {
-        wireframes : false,
-        background : '#F7F4CB',
+        wireframes : false, // true면 색 적용 안됨
+        background : '#F7F4CB', // 배경
         width : 620,
         height : 850,
     }
@@ -25,86 +26,119 @@ const render = Render.create({
 const world = engine.world;
 
 const leftWall = Bodies.rectangle(15, 395, 30, 790, {
-    isStatic : true,
-    render: { fillStyle : '#E6B143' }
+    isStatic : false,
+    render: { fillStyle : '#000000' }
 });
     
-const rightWall = Bodies.rectangle(605, 395, 30, 790, { 
-    isStatic : true, 
-    render : { 
-        fillStyle : '#6B143' 
-    }
-});
+// const rightWall = Bodies.rectangle(605, 395, 30, 790, { 
+//     isStatic : false, 
+//     render : { 
+//         fillStyle : '#6B143' 
+//     }
+// });
 
-const ground = Bodies.rectangle(310, 820, 620, 60, { 
-    isStatic : true, 
-    render : { 
-        fillStyle : '#6B143'
-    }
-});
+// const ground = Bodies.rectangle(310, 820, 620, 60, { 
+//     isStatic : false, 
+//     render : { 
+//         fillStyle : '#6B143'
+//     }
+// });
 
-const topLine = Bodies.rectangle(310, 150, 620, 2, { 
-    isStatic : true, 
-    isSensor : true,
-    render : { 
-        fillStyle : '#6B143' 
-    }
-});
+// const topLine = Bodies.rectangle(310, 150, 620, 2, { 
+//     isStatic : false, 
+//     isSensor : true,
+//     render : { 
+//         fillStyle : '#6B143' 
+//     }
+// });
 
-World.add(world, [leftWall, rightWall, ground, topLine]);
+World.add(world, [leftWall]);
+// World.add(world, [leftWall, rightWall, ground, topLine]);
 
 Render.run(render);
-Render.run(engine);
+Runner.run(engine);
 
-// 현재 과일 값을 저장할 변수 생성
-let currentBody = null;
-let currentFruit = null;
+// // 현재 과일 값을 저장할 변수 생성
+// let currentBody = null;
+// let currentFruit = null;
 
-let disableAction = false;
-function addFruit(){
-    // 과일 index 저장
-    const index = Math.floor(Math.random()*5);
-    const fruit = FRUITS[index];
-    const body = Bodies.circle(300, 50, fruit.radius, {
-        index : index,
-        isSleeping : true,
-        render  :{
-            sprite : {textrue : `${fruit.name}.png`},
-        },
-        restitution : 0.5,
-    });
+// let disableAction = false;
 
-    currentBody = body;
-    currentFruit = fruit;
-    World.add(world, body)
-}
+// function addFruit() {
+//     // 과일 index 저장
+//     const index = Math.floor(Math.random()*5);
+//     const fruit = FRUITS[index];
+//     const body = Bodies.circle(300, 50, fruit.radius, {
+//         index : index,
+//         // isSleeping : true,
+//         render  :{
+//             sprite : {texture : `${fruit.name}.png`},
+//         },
+//         restitution : 0.5,
+//     });
 
-window.onkeydown = (event) =>{
-    if(disableAction)
-        return;
+//     currentBody = body;
+//     currentFruit = fruit;
+//     World.add(world, body)
+// }
 
-    switch(event.code){
-        case "KeyA":
-            Body.setPosition(currentBody, {
-                x : currentBody.position.x - 10,
-                y : currentBody.position.y
-            })
-            break;
-        case "KeyD":
-            Body.setPosition(currentBody, {
-                x : currentBody.position.x + 10,
-                y : currentBody.position.y
-            })
-            break;
-        case "KeyS":
-            currentBody.isSleeping = false;
-            disableAction = true;
-            setTimeout(() => {
-                addFruit();
-                disableAction = false;
-            }, 1000);
-            break;
-    }
-}
+// window.onkeydown = (event) =>{
+//     if(disableAction)
+//         return;
 
-addFruit();
+//     switch(event.code){
+//         case "KeyA":
+//             Body.setPosition(currentBody, {
+//                 x : currentBody.position.x - 10,
+//                 y : currentBody.position.y
+//             })
+//             break;
+//         case "KeyD":
+//             Body.setPosition(currentBody, {
+//                 x : currentBody.position.x + 10,
+//                 y : currentBody.position.y
+//             })
+//             break;
+//         case "KeyS":
+//             currentBody.isSleeping = false;
+//             disableAction = true;
+//             setTimeout(() => {
+//                 addFruit();
+//                 disableAction = false;
+//             }, 1000);
+//             break;
+//     }
+// }
+
+// Events.on(engine, "collisionStart", (event)=>{
+//     // 콜리전 이벤트 발생시 생기는 모든 오브젝트를 비교
+//     event.pairs.forEach((collision) => {
+//         if(collision.bodyA.index == collision.bodyB.index){
+
+//             // 기존 과일의 index를 저장
+//             const index = collision.bodyA.index;
+
+//             // 충동이 일어나는 같은 과일 제거
+//             World.remove(world, [collision.bodyA, collision.bodyB]);
+
+//             // 기존 과일에서 1 증가 시킨 값을 저장
+//             const newFruit = FRUITS[index + 1];
+//             const newBody = Bodies.circle(
+//                 // 부딪친 위치의 x, y 값
+//                 collision.collision.supports[0].x,
+//                 collision.collision.supports[0].y,
+//                 newFruit.radius,
+//                 {
+//                     // 과일 index 저장
+//                     index : index + 1,
+//                     // 새로운 과일 렌더링
+//                     render : {sprite : {texture : `${newFruit.name}.png`}},
+//                 }
+//             );
+
+//             World.add(world, newBody);
+//         }
+//     });
+// });
+
+// addFruit();
